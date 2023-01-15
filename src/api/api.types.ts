b18e2b51c22e1b44fb753bc5fb8a612c01eacd31
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2023-01-15 18:48:43 +0900

    各種オブジェクトの要素に脚注を追加

--- src/api/api.types.ts
+++ src/api/api.types.ts
@@ -4,7 +4,9 @@
  * API の戻り値の原始型
  */
 interface ApiResult {
+  /** API 呼び出しに成功したときに真 */
   succeeded: boolean;
+  /** API 呼び出しに失敗した理由（失敗時のみ） */
   reason?: string;
 }
 
@@ -22,6 +24,7 @@ export type UserId = GeneralId;
  * createUser の戻り値の型
  */
 export interface CreateUserResult extends ApiResult {
+  /** 発行されたユーザ識別子 */
   userId?: UserId;
 }
 
@@ -57,7 +60,9 @@ export interface IsAcceptableResult extends ApiResult {
  * routeName で利用される保存済経路情報の型
  */
 export interface PassableName {
+  /** 経路の名前 */
   routeName: string;
+  /** 経路を利用できるとき真 */
   available: boolean;
 }
 
@@ -65,6 +70,7 @@ export interface PassableName {
  * routeName の戻り値の型
  */
 export interface RouteNameResult extends ApiResult {
+  /** 保存済み経路情報のリスト */
   passableName?: PassableName[];
 }
 
@@ -72,7 +78,9 @@ export interface RouteNameResult extends ApiResult {
  * 緯度と経度の対を表す型
  */
 export interface Position {
+  /** 北緯を正とする緯度（度数法） */
   lat: number;
+  /** 東経を正とする経度（度数法） */
   lng: number;
 }
 
@@ -80,6 +88,7 @@ export interface Position {
  * reqRoute のパラメータの型
  */
 export interface ReqRouteArg {
+  /** 経路の名前 */
   routeName: string;
 }
 
@@ -87,8 +96,11 @@ export interface ReqRouteArg {
  * reqRoute の戻り値の型
  */
 export interface ReqRouteResult extends ApiResult {
+  /** 経路 */
   route?: Position[][];
+  /** 経路に含まれる停留所のリスト */
   dest?: Position[];
+  /** 巡回経路のとき真 */
   junkai?: boolean;
 }
 
@@ -96,8 +108,11 @@ export interface ReqRouteResult extends ApiResult {
  * 通行可能領域のそれぞれの円を表す型
  */
 export interface PassableInfo {
+  /** 通行可能領域の中央座標 */
   position: Position; // XXX: 文書と実装の齟齬（実装に沿う）
+  /** 通行可能領域の半径 */
   radius: number;
+  /** 通行可能領域の識別子 */
   passableId: number;
 }
 
@@ -105,6 +120,7 @@ export interface PassableInfo {
  * reqPassable の戻り値の型
  */
 export interface ReqPassableResult extends ApiResult {
+  /** 通行可能領域情報のリスト */
   passableInfo?: PassableInfo[];
 }
 
@@ -112,6 +128,7 @@ export interface ReqPassableResult extends ApiResult {
  * astar のパラメータの型
  */
 export interface AstarArg {
+  /** 通過すべき地点のリスト */
   data: Position[];
 }
 
@@ -119,6 +136,7 @@ export interface AstarArg {
  * astar の戻り値の型
  */
 export interface AstarResult extends ApiResult {
+  /** 生成されたルート */
   route?: Position[];
 }
 
@@ -126,7 +144,9 @@ export interface AstarResult extends ApiResult {
  * ExecRoute のパラメータの型
  */
 export interface ExecRouteArg {
+  /** 実行する経路 */
   data: Position[][];
+  /** 巡回経路のとき真 */
   junkai: boolean;
 }
 
@@ -141,8 +161,11 @@ export interface ExecRouteResult extends ApiResult {
  * saveRoute のパラメータの型
  */
 export interface SaveRouteArg {
+  /** 経路の名前 */
   routeName: string;
+  /** 保存する経路 */
   data: Position[][];
+  /** 巡回経路のとき真 */
   junkai: boolean;
 }
 
@@ -150,6 +173,7 @@ export interface SaveRouteArg {
  * saveRoute の戻り値の型
  */
 export interface SaveRouteResult extends ApiResult {
+  /** 経路の名前 */
   routeName?: string;
 }
 
@@ -157,13 +181,22 @@ export interface SaveRouteResult extends ApiResult {
  * monitorCar の戻り値の型
  */
 export interface MonitorCarResult extends ApiResult {
+  /** 車の割り当てがあるときに真 */
   reserve?: boolean;
+  /** 実行している経路 */
   route?: Position[][];
+  /** 実行している経路に含まれる停留所のリスト */
   dest?: Position[];
+  /** 停留所で停まっているときに真 */
   arrival?: boolean;
+  /** 目的地に停まっているときに真 */
   finish?: boolean;
+  /** 経路の始点に到達済のときに真 */
   arrange?: boolean;
+  /** 車が正常状態であるときに真 */
   status?: boolean;
+  /** 車の現在位置 */
   nowPoint?: Position;
+  /** 車のバッテリ残量 */
   battery?: number;
 }
