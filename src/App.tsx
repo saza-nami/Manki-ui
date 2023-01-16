5f2b3b1d6eaff37ac56eba5782a4d253e29c8e09
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2023-01-16 12:17:42 +0900

    初期状態から第一状態までの遷移を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import Main from 'Main';
+import * as Manki from 'api/manki';
+import Swal from 'sweetalert2';
+
+export const userIdContext = React.createContext({} as {
+  userId: Manki.UserId | undefined;
+});
 
 function App() {
-  const [phase, setPhase] = React.useState<string>('1');
+  const [phase, setPhase] = React.useState<string>('0');
+  const [userId, setUserId] = React.useState<Manki.UserId>();
+  
+  async function onLoad() {
+    const userId = await Manki.createUser();
+    if (userId instanceof Error) {
+      Swal.disableButtons();
+      Swal.fire({
+        titleText: 'エラーが発生しました',
+        text: userId.message + '続行するにはリロードしてください。',
+        icon: 'error',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false,
+        showConfirmButton: false,
+        willClose: () => window.location.reload(),
+      });
+      return;
+    }
+    setUserId(userId);
+    setPhase('1');
+  }
+  
+  const didLogRef = React.useRef(false);
+  React.useEffect(() => {
+    if (!didLogRef.current) {
+      didLogRef.current = true;
+      onLoad();
+    }
+  }, []);
 
   return (
-    <Main phase={phase} setPhase={setPhase}/>
+    <userIdContext.Provider value={{userId}}>
+      <Main phase={phase} setPhase={setPhase} />
+    </userIdContext.Provider>
   );
 }
 
