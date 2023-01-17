332b062d7fd944ca5320599bfbde00a05a6c6f6d
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2023-01-17 12:19:45 +0900

    第一状態から第二状態に値を転送

--- src/components/forms/SecondForm.tsx
+++ src/components/forms/SecondForm.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import * as App from 'App';
+import * as Main from 'Main';
+import * as Manki from 'api/manki';
+import * as Form from 'components/Form';
+import * as FirstForm from './FirstForm';
+import generateIcon from 'util/icon';
+import L, { map } from 'leaflet';
 import './forms.css';
 
 interface Props {
@@ -8,6 +15,12 @@ interface Props {
 function SecondForm({
     setPhase,
 }: Props) {
+    const userId = React.useContext(App.userIdContext).userId as Manki.UserId;
+    const map = React.useContext(Main.mapRefContext).mapRef.current;
+    const routeInfo = React.useContext(Form.phaseContext).shareRef.current as FirstForm.RouteInfo;
+    const markers = [] as L.Marker[];
+    let routeLine = L.polyline([]) as L.Polyline;
+
     function editRoute() {
         setPhase('1');
     }
@@ -15,6 +28,31 @@ function SecondForm({
     function executeRoute() {
         setPhase('3');
     }
+
+    function showRoute() {
+        routeInfo.route.forEach((subRoute, i) => {
+            const icon = generateIcon('blue', String(i+1));
+            const marker = L.marker(subRoute[0], { icon });
+            marker.addTo(map);
+            markers.push(marker);
+        });
+        if (!routeInfo.junkai) {
+            const icon = generateIcon('blue', String(routeInfo.route.length+1));
+            const marker = L.marker(routeInfo.route.at(-1)?.at(-1) as Manki.Position, { icon });
+            marker.addTo(map);
+            markers.push(marker);
+        }
+        routeLine = L.polyline(routeInfo.route, { weight: 10, color: 'green' });
+        routeLine.addTo(map);
+    }
+
+    const didLogRef = React.useRef(false);
+    React.useEffect(() => {
+        if (!didLogRef.current) {
+            didLogRef.current = true;
+            showRoute();
+        }
+    }, []);
 
     return (
         <form id="secondForm" className="the-form" onSubmit={(e) => e.preventDefault()}>
