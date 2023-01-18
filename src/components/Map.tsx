b7159f6dfa0106c07b46f973a802523b13869f4f
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2023-01-18 14:46:48 +0900

    マップの初期設定を変更

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -11,12 +11,12 @@ function Map({
     mapRef,
 }: Props) {
     return (
-        <ReactLeaflet.MapContainer center={[35,138]} zoom={12} ref={mapRef}> {/* XXX */}
+        <ReactLeaflet.MapContainer center={[35.64143255555,139.74135747222]} zoom={12} ref={mapRef}>
             <ReactLeaflet.TileLayer
                 attribution="<a href='https://maps.gsi.go.jp/development/ichiran.html'>地理院タイル</a>"
                 url="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png"
                 maxNativeZoom={18}
-                maxZoom={18}
+                maxZoom={21}
                 minNativeZoom={5}
                 minZoom={5}
             />
